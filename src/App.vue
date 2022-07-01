ca8af8b293bfda1af35b33422e088dd0c048c27e
Alexandr Androsov <alexandr.v.androsov@gmail.com>
2022-07-01 18:28:31 +0300

    Сортровка через computed

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
       />
     </post-modal>
     <post-list
-        :posts="posts"
+        :posts="sortedPosts"
         @remove="removePost"
         v-if="!isPostsLoading"
     />
@@ -78,14 +78,20 @@ export default {
   mounted() {
     this.fetchPosts();
   },
-  watch: {
-    selectedSort(newValue) {
-      this.posts.sort((post1, post2) => {
-        return post1[newValue]?.localeCompare(post2[newValue]);
+  computed: {
+    sortedPosts() {
+      return[...this.posts].sort((post1, post2) => {
+        return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]);
       })
     }
-
-  }
+  },
+  // watch: {
+  //   selectedSort(newValue) {
+  //     this.posts.sort((post1, post2) => {
+  //       return post1[newValue]?.localeCompare(post2[newValue]);
+  //     })
+  //   }
+  // }
 }
 </script>
 
